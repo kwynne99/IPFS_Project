4602b58e8b1c22a533f9d75776150b3418d61cf7
Kyle Wynne <kylemwynne@outlook.com>
2020-11-24 11:48:23 -0500

    Create receiver.h

---
+++ receiver.h
@@ -0,0 +1,125 @@
+#pragma once
+#include <WinSock2.h>
+#include <stdio.h>
+#include <iostream>
+#include <WS2tcpip.h>
+#include <ctime>
+#include <thread>
+#include <string>
+#include <vector>
+#include "helper.h"
+#include "sender.h"
+#include "fs.h"
+#pragma comment(lib, "Ws2_32.lib")
+#define BUFFER_SIZE 256
+#define HOSTNAME "127.0.0.1"
+
+
+class Receiver {
+private:
+	//std::string receiverName = "";
+	std::string receiverPort = "";
+	int sockfd = 0, newsockfd = 0, portno = 0;
+	socklen_t clientLen;
+	struct sockaddr_in serv_addr, client_addr;
+	char buffer[256];
+	struct CLIENTLIST clientList;
+	std::vector<int> peerList;
+public:
+	//Receiver(std::string, std::string);
+	Receiver(std::string);
+	void startReceiver();
+};
+
+/*Receiver::Receiver(std::string rName, std::string rPort) {
+	//this->receiverName = rName;
+	this->receiverPort = rPort;
+}*/
+
+Receiver::Receiver(std::string rPort) {
+	this->receiverPort = rPort;
+}
+
+void Receiver::startReceiver() {
+	// Create FAT
+	discEmulator sandisc = discEmulator();
+	WSADATA wsaData;
+	int iResult;
+	int totalReceived = 0, totalSent = 0;
+	int i;
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup Failed: %d\n", iResult);
+		exit(0);
+	}
+
+	struct addrinfo* result = NULL, * ptr = NULL, hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET; // IPv4
+	hints.ai_socktype = SOCK_DGRAM; // Stream socket (TCP)
+	hints.ai_protocol = IPPROTO_UDP; // Specifies UDP protocol
+	hints.ai_flags = AI_PASSIVE; // Return socket address structure in call to bind
+
+	iResult = getaddrinfo(HOSTNAME, receiverPort.c_str(), &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed receiver: %d\n", iResult);
+		WSACleanup();
+		exit(0);
+	}
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("Error at socket(), %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		exit(0);
+	}
+
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		printf("Bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		exit(0);
+	}
+//	freeaddrinfo(result); // No longer needed
+
+	printf("** Receiver started **\n");
+	printf("Hostname: %s\nPort#:%s\n", HOSTNAME, receiverPort.c_str());
+
+	SOCKET ClientSocket; // Temporary socket used to accept connections.
+	struct sockaddr_in SenderAddr;
+	int SenderAddrSize = sizeof SenderAddr;
+	std::string temp;
+	do {
+		iResult = recvfrom(ListenSocket, buffer, BUFFER_SIZE, 0, (SOCKADDR *)&SenderAddr, (int*)&SenderAddrSize);
+		temp = buffer;
+		if (iResult > 0) {
+			if (!strcmp(buffer, "exit")) {
+				std::cout << SenderAddr.sin_port << " has shutdown." << std::endl; // Remove this node from table.
+			}
+			else if (!temp.compare(0, 5, "touch")) {
+				// Get ready for incoming block and then allocate.
+				iResult = recvfrom(ListenSocket, buffer, BUFFER_SIZE, 0, (SOCKADDR*)&SenderAddr, (int*)&SenderAddrSize);
+				// After allocating block, send location back.
+				int freeBlock = sandisc.getTopFreeBlockIndex();
+				std::string newFat;
+				sandisc.allocate(buffer, sandisc.getTopFreeBlockIndex());
+				printf("Successfully allocated block.\n");
+				sendto(ListenSocket, (char*)&freeBlock, sizeof freeBlock, 0, (SOCKADDR*)&SenderAddr, (int*)&SenderAddrSize);
+				recvfrom(ListenSocket, buffer, BUFFER_SIZE, 0, (SOCKADDR*)&SenderAddr, (int*)&SenderAddrSize);
+				newFat = buffer;
+				sandisc.updateFat(newFat);
+				printf("FAT updated.\n");
+			}
+			else {
+				std::cout << SenderAddr.sin_port << ": " << buffer << std::endl;
+			}
+		}
+	} while (iResult > 0);
+}
